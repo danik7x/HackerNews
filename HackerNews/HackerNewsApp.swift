c9202d3a139d9098972a4ec34e96921e61e07702
Daniel <neca.daniel@icloud.com>
2024-03-26 14:19:56 +0100

    Initial commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Daniel Necula on 26.03.24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
